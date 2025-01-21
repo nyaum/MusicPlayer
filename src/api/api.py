be0fa7dea790f4f26db77a7db859ebc44bb4f6da
문현준 <nyaum@kakao.com>
2025-01-21 15:44:11 +0900

    python 으로 백엔드 구축함

---
+++ src/api/api.py
@@ -0,0 +1,88 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import subprocess
+import os
+import uuid
+import json
+import chardet
+
+app = Flask(__name__)
+CORS(app)
+
+# 다운로드 디렉토리 설정
+DOWNLOAD_DIR = './downloads'
+
+# 다운로드 디렉토리 생성
+if not os.path.exists(DOWNLOAD_DIR):
+    os.makedirs(DOWNLOAD_DIR)
+
+@app.route('/download', methods=['POST'])
+def download_video():
+    data = request.json
+    url = data.get('url')
+
+    if not url:
+        return jsonify({"error": "URL is required"}), 400
+
+    try:
+        # 임시 파일 이름 생성
+        filename = str(uuid.uuid4())
+        filepath = os.path.join(DOWNLOAD_DIR, f'{filename}.%(ext)s')
+
+        # yt-dlp 명령어 실행 (기본 검색 옵션 추가)
+        result = subprocess.run(
+            ['yt-dlp', 
+             '--write-info-json',
+             '--skip-download',
+             '--flat-playlist',
+             '--quiet',
+             '--ignore-errors',
+             "--default-search", "ytsearch",
+             '-x', 
+             '-vU', 
+             '--audio-format', 'mp3', 
+             url, '-o', filepath],
+            capture_output=True,
+            text=True
+        )
+
+        if result.returncode == 0:
+            downloaded_filepath = os.path.join(DOWNLOAD_DIR, filename)
+            metadata_filepath = downloaded_filepath + '.info.json'
+            print(metadata_filepath)
+            if os.path.exists(metadata_filepath):
+                with open(metadata_filepath, 'r', encoding='utf-8') as f:
+                    metadata = json.load(f)
+                
+                # 특정 형식만 저장
+                formats = metadata.get('formats', [])
+                format_249 = next((f for f in formats if f.get('format_id') == '249'), None)
+                
+                if format_249:
+                    track_info = {
+                        "track": metadata.get('title', 'Unknown Track'),
+                        "artist": metadata.get('uploader', 'Unknown Artist'),
+                        "album": metadata.get('album', 'Unknown Album'),
+                        "albumCover": metadata.get('thumbnail', ''),
+                        "audio": format_249["url"],
+                    }
+
+                    print(track_info)
+                    
+                    return jsonify({"message": "Download successful", "playlist": track_info})
+                else:
+                    return jsonify({"error": "Failed to find format 249"}), 500
+            else:
+                return jsonify({"error": "Metadata file not found"}), 500
+
+        else:
+            return jsonify({"error": result.stderr}), 500
+        
+    except FileNotFoundError:
+        return jsonify({"error": "yt-dlp not found. Ensure it is installed and in the PATH."}), 500
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+if __name__ == '__main__':
+    app.run()
